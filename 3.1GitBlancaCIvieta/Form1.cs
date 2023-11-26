79ec765656b8a8f2713f16481fb3f4d9810525b1
UsuarioB <blancivi@gmail.com>
2023-11-26 12:18:59 +0100

    Corrección código

--- 3.1GitBlancaCIvieta/Form1.cs
+++ 3.1GitBlancaCIvieta/Form1.cs
@@ -24,6 +24,7 @@ namespace _3._1GitBlancaCIvieta
 
         private void Calcular_Click(object sender, EventArgs e)
         {
+            //Blanca Felisa Civieta Bermejo 23/24
             string textoTelegrama;
             char tipoTelegrama = ' ';
             int numPalabras = 0;
@@ -34,21 +35,33 @@ namespace _3._1GitBlancaCIvieta
             // telegrama urgente? 
             if (cbUrgente.Checked)
                 tipoTelegrama = 'u';
+            else
+                tipoTelegrama = 'o';
             //Obtengo el número de palabras que forma el telegrama 
-            numPalabras = textoTelegrama.Length;
+            numPalabras = 0;
+            for (int i = 0; i < textoTelegrama.Length; i++) 
+            {
+                if (textoTelegrama[i] == ' ' || textoTelegrama[i] == '.' || textoTelegrama[i] == ',')
+                {
+                    numPalabras++;
+                }
+            }
+            int longCadena = textoTelegrama.Length - 1;
+            if (textoTelegrama[longCadena] != '.' || textoTelegrama[longCadena] != ' ')
+                numPalabras++;
             //Si el telegrama es ordinario 
             if (tipoTelegrama == 'o')
                 if (numPalabras <= 10)
-                    coste = 25;
+                    coste = 2.5;
                 else
-                    coste = 0.5 * numPalabras;
+                    coste = 0.5 + (0.5 *( numPalabras -10));
             else
             //Si el telegrama es urgente 
             if (tipoTelegrama == 'u')
                 if (numPalabras <= 10)
                     coste = 5;
                 else
-                    coste = 5 + 0.75 * (numPalabras - 10);
+                    coste = 5 + (0.75 * (numPalabras - 10));
             else
                 coste = 0;
             txtPrecio.Text = coste.ToString() + " euros";
