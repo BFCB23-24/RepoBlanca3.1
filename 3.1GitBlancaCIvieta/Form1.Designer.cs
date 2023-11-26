96ad3024057c4c5d22f851a99c50e0c3496cedfb
BLANCA FELISA CIVIETA BERMEJO <blacivber@alu.edu.gva.es>
2023-11-26 11:45:44 +0100

    Creación formulario y código

--- 3.1GitBlancaCIvieta/Form1.Designer.cs
+++ 3.1GitBlancaCIvieta/Form1.Designer.cs
@@ -28,13 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelegrama = new System.Windows.Forms.TextBox();
+            this.cbUrgente = new System.Windows.Forms.CheckBox();
+            this.Coste = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.Calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Práctica GIT 3.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Blanca Felisa Civieta Bermejo 23/24";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(61, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Texto";
+            // 
+            // txtTelegrama
+            // 
+            this.txtTelegrama.Location = new System.Drawing.Point(62, 156);
+            this.txtTelegrama.Multiline = true;
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(535, 104);
+            this.txtTelegrama.TabIndex = 3;
+            // 
+            // cbUrgente
+            // 
+            this.cbUrgente.AutoSize = true;
+            this.cbUrgente.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbUrgente.Location = new System.Drawing.Point(64, 287);
+            this.cbUrgente.Name = "cbUrgente";
+            this.cbUrgente.Size = new System.Drawing.Size(100, 20);
+            this.cbUrgente.TabIndex = 4;
+            this.cbUrgente.Text = "¿Urgente?";
+            this.cbUrgente.UseVisualStyleBackColor = true;
+            // 
+            // Coste
+            // 
+            this.Coste.AutoSize = true;
+            this.Coste.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Coste.Location = new System.Drawing.Point(70, 356);
+            this.Coste.Name = "Coste";
+            this.Coste.Size = new System.Drawing.Size(47, 16);
+            this.Coste.TabIndex = 5;
+            this.Coste.Text = "Coste";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(131, 356);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(72, 22);
+            this.txtPrecio.TabIndex = 6;
+            this.txtPrecio.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Calcular
+            // 
+            this.Calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calcular.Location = new System.Drawing.Point(440, 314);
+            this.Calcular.Name = "Calcular";
+            this.Calcular.Size = new System.Drawing.Size(130, 64);
+            this.Calcular.TabIndex = 7;
+            this.Calcular.Text = "Calcular";
+            this.Calcular.UseVisualStyleBackColor = true;
+            this.Calcular.Click += new System.EventHandler(this.Calcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(697, 450);
+            this.Controls.Add(this.Calcular);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.Coste);
+            this.Controls.Add(this.cbUrgente);
+            this.Controls.Add(this.txtTelegrama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTelegrama;
+        private System.Windows.Forms.CheckBox cbUrgente;
+        private System.Windows.Forms.Label Coste;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button Calcular;
     }
 }
 
